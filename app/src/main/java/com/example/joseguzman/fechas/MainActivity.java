857f6ef10705e7ac3f89509b2f1d1d6999b438dc
joseg <2guzmanjose@gmail.com>
2018-10-04 16:47:19 -0300

    Validacion de campo agregada. Solo permite dato de entrada bajo formato de fecha válida dd/mm/yyy.

--- app/src/main/java/com/example/joseguzman/fechas/MainActivity.java
+++ app/src/main/java/com/example/joseguzman/fechas/MainActivity.java
@@ -7,7 +7,6 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
-
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
@@ -15,7 +14,8 @@ public class MainActivity extends AppCompatActivity {
     EditText etFechaNacimiento;
     Button btnBuscarDia;
     TextView tvDiaSemNac;
-
+    SimpleDateFormat formatofecha = new SimpleDateFormat("dd/MM/yyyy");
+    SimpleDateFormat formatodia = new SimpleDateFormat("EEEE");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,24 +26,22 @@ public class MainActivity extends AppCompatActivity {
         btnBuscarDia = (Button) findViewById(R.id.btnBuscarDia);
         tvDiaSemNac = (TextView) findViewById(R.id.tvDiaSemNac);
 
-
-
         btnBuscarDia.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
-                SimpleDateFormat formato = new SimpleDateFormat("dd/MM/yyyy");
                 String fechaStr = etFechaNacimiento.getText().toString();
-                SimpleDateFormat formatodia = new SimpleDateFormat("EEEE");
+
                 try{
-                    Date fecha = formato.parse(fechaStr);
+                    formatofecha.setLenient(false);
+                    Date fecha = formatofecha.parse(fechaStr);
                     tvDiaSemNac.setText(formatodia.format(fecha));
-            }catch (Exception e){
+                }catch (Exception e){
+                    etFechaNacimiento.setError("El formato de fecha debe ser dd/mm/aaaa");
                     Toast.makeText(getBaseContext(),e.getMessage(),Toast.LENGTH_SHORT).show();
                 }
             }
         });
     }
-
 
 }
